1bd672f29077e0599e1f1fed3372973720225bb8
Edward Hobbs <edward24hobbs@outlook.com>
2016-08-03 21:42:47 +0100

    finished dvorak removal

--- keyboards/preonic/keymaps/hoobs/keymap.c
+++ keyboards/preonic/keymaps/hoobs/keymap.c
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = {
     {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
     {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL},
-    {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______},
+    {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______,  _______, _______},
     {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
   }
@@ -151,10 +151,6 @@ float tone_startup[][2] = {
 
 float tone_qwerty[][2]     = {
   {NOTE_C5, 20}
-};
-
-float tone_dvorak[][2]     = {
-  {NOTE_G5, 20}
 };
 
 float tone_colemak[][2]    = {
@@ -188,15 +184,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       PLAY_NOTE_ARRAY(tone_colemak, false, 0);
       #endif
       persistant_default_layer_set(1UL<<_COLEMAK);
-    }
-    return false;
-    break;
-    case DVORAK:
-    if (record->event.pressed) {
-      #ifdef AUDIO_ENABLE
-      PLAY_NOTE_ARRAY(tone_dvorak, false, 0);
-      #endif
-      persistant_default_layer_set(1UL<<_DVORAK);
     }
     return false;
     break;
